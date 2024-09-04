117f7fd9fb842f29e852d9b3b6875d3825714621
KOKELKA Sasha <Gewalt70@gmail.com>
2024-09-04 09:44:12 +0200

    2nd commit

---
+++ frontend/navbar.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <script src="https://cdn.tailwindcss.com"></script> 
+  <script src="https://unpkg.com/flowbite@1.5.1/dist/flowbite.js"></script>
+</head>
+
+<body>
+
+<header class="text-gray-700 body-font border-b border-gray-200">
+    <div class="container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center">
+        <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href="index.php"></a>
+            <nav class="flex items-center">
+                <a href="index.php"><img src="../frontend/images/gifi.png" class="w-15 h-10 mr-5 hover:text-gray-900"></a>
+                
+                <!-- Centrage du texte et de l'icône -->
+                <a href="ean.php" class="mr-5 hover:text-gray-900">EAN</a>
+                <a href="Palettes.php" class="mr-5 hover:text-gray-900">Palettes</a>
+                <a href="gestiontech.php" class="mr-5 hover:text-gray-900">Gestion</a>
+                <a href="assignes.php" class="mr-5 hover:text-gray-900">Voyages</a>
+                <a href="autrestickets.php" class="mr-5 hover:text-gray-900">Articles</a>
+            </nav>
+    </div>
+</header>
+
+</body>
+</html>
